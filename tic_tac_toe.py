6588f4527b8d3df1486eb4c72037566c2194ed6b
NikhilSinghVIT <nikhilsingh.ns2014@gmail.com>
2020-04-19 14:24:01 +0530

    Added misc functions and play_game() function

--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -202,3 +202,74 @@ class Human:
         pass
 
 
+def get_state_hash_and_winner(env, i=0, j=0):
+    results = []
+    for v in (0, env.x, env.o):
+        if j == 2:
+            if i == 2:
+                state = env.get_state()
+                ended = env.game_over(force_recalculate=True)
+                winner = env.winner
+                results.append((state, winner, ended))
+            else:
+                results += get_state_hash_and_winner(env, i + 1, 0)
+        else:
+            results += get_state_hash_and_winner(env, i, j + 1)
+
+    return results
+
+
+def initialV_x(env, state_winner_triples):
+    V = np.zeros(env.num_states)
+    for state, winner, ended in state_winner_triples:
+        if ended:
+            if winner == env.x:
+                v = 1
+            else:
+                v = 0
+        else:
+            v = 0.5
+        V[state] = v
+    return V
+
+
+def initialV_o(env, state_winner_triples):
+    V = np.zeros(env.num_states)
+    for state, winner, ended in state_winner_triples:
+        if ended:
+            if winner == env.o:
+                v = 1
+            else:
+                v = 0
+        else:
+            v = 0.5
+        V[state] = v
+    return V
+
+
+def play_game(p1, p2, env, draw=False):
+    current_player = None
+    while not env.game_over():
+        if current_player == p1:
+            current_player = p2
+        else:
+            current_player = p1
+
+    # draw the board before the user who wants to see it makes a move
+        if draw:
+            if draw == 1 and current_player == p1:
+                env.draw_board()
+            if draw == 2 and current_player == p2:
+                env.draw_board()
+
+    # current player makes a move
+        current_player.take_action(env)
+
+    # update state histories
+        state = env.get_state()
+        p1.update_state_history(state)
+        p2.update_state_history(state)
+    if draw:
+      env.draw_board()
+    p1.update(env)
+    p2.update(env)
